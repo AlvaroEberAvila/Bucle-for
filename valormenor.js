7d11b340adf732bb70a6c5da8f29c2c24b06d68b
Alvaro Avila <alvaroeg01@gmail.com>
2022-06-27 16:22:26 -0300

    array

---
+++ valormenor.js
@@ -0,0 +1,13 @@
+for(i=0; i<22; i++){
+let numero = Math.random()*100;
+numero = Math.round(numero);
+let arr = [numero];
+document.write(arr + ", ");
+}
+let funcion = (arr) => {
+    arr[numero].sort();
+    document.write(arr);
+}
+
+funcion(arr);
+
